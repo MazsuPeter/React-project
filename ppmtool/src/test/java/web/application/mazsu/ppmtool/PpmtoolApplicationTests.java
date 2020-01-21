e1345ed02d7ca053e2f70be33a0c8206c79d0945
mpeti <mazsu123@gmail.com>
2020-01-21 22:15:23 +0100

    first commit

---
+++ ppmtool/src/test/java/web/application/mazsu/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package web.application.mazsu.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
